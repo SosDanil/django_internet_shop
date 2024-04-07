af5164943f36832199da9e87d557ac759937a7d8
Danil Klimenko <sos.danil@yandex.ru>
2024-04-07 13:32:54 +0500

    настроил отображение моделей в админке

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
-# Register your models here.
+from catalog.models import Category, Product
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['pk', 'name', 'description']
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['pk', 'name', 'price', 'category', 'description']
+    list_filter = ['category']
+    search_fields = ['name', 'description']
