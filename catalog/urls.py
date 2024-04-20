39b0005db359317f5f866edd91f2474c6fe5cfda
Danil Klimenko <sos.danil@yandex.ru>
2024-04-20 14:13:56 +0500

    Сделал задание 1 (FBV -> CBV) и создал новое приложение articles

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,12 +1,12 @@
 from django.urls import path
 
 from catalog.apps import CatalogConfig
-from catalog.views import home, contacts, product
+from catalog.views import ProductListView, ProductDetailView, ContactsPageView
 
 app_name = CatalogConfig.name
 
 urlpatterns = [
-    path('', home),
-    path('contacts/', contacts, name='contacts'),
-    path('product/<int:pk>', product, name='product')
+    path('', ProductListView.as_view(), name='home'),
+    path('contacts/', ContactsPageView.as_view(), name='contacts'),
+    path('product/<int:pk>', ProductDetailView.as_view(), name='product')
 ]
--- catalog/views.py
+++ catalog/views.py
@@ -1,32 +1,35 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView, TemplateView
 
 from catalog.models import Product
 
 
-# Create your views here.
-def home(request):
-    context = {
-        'title': 'Главная',
-        'object_list': Product.objects.all(),
+class ProductListView(ListView):
+    model = Product
+    template_name = 'catalog/home.html'
+    extra_context = {
+        'title': 'Главная'
     }
-    return render(request, 'catalog/home.html', context)
 
 
-def contacts(request):
-    context = {
-        'title': 'Контакты'
-    }
-    if request.method == 'POST':
-        name = request.POST.get('name')
-        phone = request.POST.get('phone')
-        message = request.POST.get('message')
-        print(f'{name} ({phone}): {message}')
-    return render(request, 'catalog/contacts.html', context)
-
-
-def product(request, pk):
-    context = {
-        'object': Product.objects.get(pk=pk),
+class ProductDetailView(DetailView):
+    model = Product
+    template_name = 'catalog/product.html'
+    extra_context = {
         'title': 'Описание продукта'
     }
-    return render(request, 'catalog/product.html', context)
+
+
+class ContactsPageView(TemplateView):
+    template_name = 'catalog/contacts.html'
+    extra_context = {
+        'title': 'Контакты'
+    }
+
+    def dispatch(self, request, *args, **kwargs):
+        if request.method == 'POST':
+            name = request.POST.get('name')
+            phone = request.POST.get('phone')
+            message = request.POST.get('message')
+            print(f'{name} ({phone}): {message}')
+        return render(request, 'catalog/contacts.html', context=self.extra_context)
